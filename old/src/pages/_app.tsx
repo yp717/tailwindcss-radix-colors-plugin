b078f5665cf992558b953d1c0416904eb6422f0a
Yannis Panagis <yannis.panagis1998@gmail.com>
2023-05-29 08:25:41 +0100

    feat(turborepo): added turborepo to tailwind radix plugin repository

---
+++ old/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { domAnimation, LazyMotion } from "framer-motion";
+
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <LazyMotion features={domAnimation}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </LazyMotion>
+  );
+}
