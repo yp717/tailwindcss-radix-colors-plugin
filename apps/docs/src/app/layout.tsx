8bb94dc06cef40a5a4908a30535b5f1e6919c3a6
Yannis Panagis <yannis@behaviourlab.com>
2023-06-04 19:45:48 +0100

    feat(og-img): added open graph image for social previews

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,9 +1,34 @@
+import { Metadata } from 'next'
+
 import PageLayout from '../components/navigation/PageLayout'
 import '../styles/globals.css'
 import Providers from './providers'
 
 interface IRootLayoutProps {
   children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: {
+    default: 'tailwindcss-radix-colors-plugin',
+    template: '%s | tailwindcss-radix-colors-plugin',
+  },
+  description: 'Seamless integratation for Radix Colors into TailwindCSS',
+  openGraph: {
+    title: 'tailwindcss-radix-colors-plugin',
+    description: 'Seamless integratation for Radix Colors into TailwindCSS',
+    url: 'https://tailwind-radix-plugin-docs.vercel.app/',
+    siteName: 'tailwindcss-radix-colors-plugin',
+    images: [
+      {
+        url: 'https://tailwind-radix-plugin-docs.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
 }
 
 const RootLayout = ({ children }: IRootLayoutProps) => {
