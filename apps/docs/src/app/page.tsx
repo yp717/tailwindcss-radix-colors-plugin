1020a9949fa42beefd112085c84a7fe1ac114ce0
Yannis Panagis <yannis@behaviourlab.com>
2023-06-04 15:44:14 +0100

    feat(mobile-menu): added mobile menu to docs site, adjusted font sizes and spacing

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -4,9 +4,9 @@ import MiniPalettePreview from '../components/palette/MiniPalettePreview'
 const Page = () => {
   return (
     <div>
-      <div className="absolute top-0 left-0 z-0" />
-      <div className="relative h-full flex flex-col gap-4 text-center items-center h-full w-full z-10">
-        <p className="text-gray-1200 font-bold text-3xl md:text-5xl lg:text-9xl my-4 mt-20">
+      <div className="absolute top-0 left-0" />
+      <div className="relative h-full flex flex-col gap-4 text-center items-center h-full w-full">
+        <p className="text-gray-1200 font-bold text-7xl md:text-8xl lg:text-9xl my-4 mt-20">
           <span className="italic text-transparent bg-clip-text bg-gradient-to-r from-crimsonDark-1000 to-amberDark-900">
             Elevate
           </span>{' '}
@@ -22,7 +22,7 @@ const Page = () => {
         </p>
         <Link
           href="/get-started"
-          className="rounded-lg border-4 text-grayLight-1200 border-grayLight-1000 bg-slateLight-100 w-min whitespace-nowrap px-6 py-2 z-50 no-underline mb-20"
+          className="rounded-lg border-4 text-grayLight-1200 border-grayLight-1000 bg-slateLight-100 w-min whitespace-nowrap px-6 py-2 no-underline mb-20"
         >
           Get Started
         </Link>
--- apps/docs/src/components/navigation/PageLayout.tsx
+++ apps/docs/src/components/navigation/PageLayout.tsx
@@ -19,7 +19,7 @@ const PageLayout = ({ children }: IPageLayoutProps) => {
       <DesktopSidebar className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:w-80 lg:flex lg:flex-col" />
       <main className="relative pl-0 lg:pl-80 min-h-screen w-full flex flex-col">
         <DesktopHeader className="hidden lg:flex lg:sticky top-0 left-80 z-50 h-20 w-full" />
-        <div className="max-w-4xl mx-auto p-4 my-8 h-full prose dark:prose-invert w-full">
+        <div className="max-w-4xl mx-auto p-4 my-32 lg:my-8 h-full prose dark:prose-invert w-full">
           {children}
         </div>
       </main>
